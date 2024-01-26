a3df27a2db34a52daab8b71249099e111656f42e
LuizSampaio-cpu <luizhorta2910@gmail.com>
2024-01-26 16:06:27 -0300

    add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms';
 
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { TabViewModule } from 'primeng/tabview';
-import { TooltipModule } from 'primeng/tooltip';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputMaskModule } from 'primeng/inputmask'
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CalendarModule } from 'primeng/calendar';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
 
-    ButtonModule,
-    InputTextModule,
-    TableModule,
-    TabViewModule,
-    TooltipModule,
-    CalendarModule,
-    BrowserAnimationsModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputNumberModule,
-    InputMaskModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
 
   ],
   providers: [],
